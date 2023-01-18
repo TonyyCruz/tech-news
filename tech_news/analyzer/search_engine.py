2297d23a555f75958de3264e36710e2c46206bf3
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-01-18 19:52:52 -0300

    feat: requisito 7 - implementa a funcao "search_by_date" recebe como parametro uma data no formato yyyy-mm-dd e faz uma busca no banco de dados por noticias da mesma data no formato dd-mm-yyyy, retornando uma lista de tuplas contendo o titulo e o link da noticia

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,38 @@
 from tech_news.database import search_news
 import re
+import datetime
+
+
+# Recebe uma lista de dict e devolve uma lista de tuplas
+def extract_title_and_link(dict_list):
+    return [(news["title"], news["url"]) for news in dict_list]
 
 
 # Requisito 6
 def search_by_title(title):
     query = {"title": re.compile(title, re.IGNORECASE)}
     news_searched = search_news(query)
-    response = [(news["title"], news["url"]) for news in news_searched]
+    response = extract_title_and_link(news_searched)
+
     return response
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    print(date)
+    try:
+        date_formated = (
+            datetime
+            .datetime
+            .strptime(date, "%Y-%m-%d")
+            .strftime("%d/%m/%Y")
+        )
+
+        query = {"timestamp": date_formated}
+        news_searched = search_news(query)
+        return extract_title_and_link(news_searched)
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
