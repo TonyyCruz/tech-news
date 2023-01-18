71610ba2f330600e7f13bf0edbdc72398e293498
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-01-18 18:04:40 -0300

    feat: requisito 5 - implementa a funcao "get_tech_news" essa funcao recebe como parametro a quantidade de novas noticias que deseja-se pegar dentre as noticias adiciona essas noticias no banco de dados e as retorna na funcao

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,9 @@
 import requests
 import time
 from parsel import Selector
+from .database import (
+    create_news,
+)
 
 
 # Requisito 1
@@ -31,7 +34,7 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
-def format_text(text_paragraph):
+def format_paragraph(text_paragraph):
     selector = Selector(text=text_paragraph)
     paragraph_data = selector.css("p *::text").getall()
     paragraph_text = "".join(paragraph_data).replace("\xa0", "")
@@ -44,9 +47,9 @@ def scrape_news(html_content):
     selector = Selector(text=html_content)
 
     first_paragraph = selector.css("div.entry-content p").get()
-    summary = format_text(first_paragraph)
+    summary = format_paragraph(first_paragraph)
 
-    response = {
+    return {
       "url": selector.css("link[rel='canonical']::attr(href)").get(),
       "title": selector.css("h1.entry-title::text").get().replace("\xa0", ""),
       "timestamp": selector.css("li.meta-date::text").get(),
@@ -56,11 +59,21 @@ def scrape_news(html_content):
       "tags": selector.css("section.post-tags li a::text").getall(),
       "category": selector.css("div.entry-details span.label::text").get(),
     }
-    # print("RESPONSE ==>>", response)
-
-    return response
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = "https://blog.betrybe.com/"
+    news_links_list = []
+    while len(news_links_list) < amount:
+        html_text = fetch(url)
+        news_links_list.extend(scrape_updates(html_text))
+        url = scrape_next_page_link(html_text)
+
+    news_list = []
+    for index in range(amount):
+        news_html = fetch(news_links_list[index])
+        new_news = scrape_news(news_html)
+        news_list.append(new_news)
+    create_news(news_list)
+    return news_list
